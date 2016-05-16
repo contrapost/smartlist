016aacb3d2e06b7727938c986681d06196fecece
Anita Ilieva <anitailieva@Anitas-MacBook-Pro.local>
2016-05-16 14:39:39 +0200

    Changed api key for the soultion

--- smartlist/db/schema.rb
+++ smartlist/db/schema.rb
@@ -16,8 +16,8 @@ ActiveRecord::Schema.define(version: 20160504152955) do
   create_table "favorite_stores", force: :cascade do |t|
     t.text     "name"
     t.string   "address"
-    t.decimal  "latitude"
-    t.decimal  "longitude"
+    t.float    "latitude"
+    t.float    "longitude"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
