8e672a6eed8e5bfe235381aa7e041d968b9f1963
Alexander Shipunov <shiale14@student.westerdals.no>
2016-05-19 17:19:20 +0200

    issue 11, fix generating of email addresses for twitter authentication (twitter doesn't share user mail addresses

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -28,15 +28,29 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     end
   end
 
-  def twitter
-    # You need to implement the method below in your model (e.g. app/models/user.rb)
-    @user = User.from_omniauth(request.env["omniauth.auth"])
+  # def twitter
+  #   # You need to implement the method below in your model (e.g. app/models/user.rb)
+  #   @user = User.from_omniauth(request.env["omniauth.auth"])
+  #
+  #   if @user.persisted?
+  #     flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Twitter"
+  #     sign_in_and_redirect @user, :event => :authentication
+  #   else
+  #     session["devise.twitter_data"] = request.env["omniauth.auth"]
+  #     redirect_to new_user_registration_url
+  #   end
+  # end
 
+  def twitter
+    auth = env["omniauth.auth"]
+
+    @user = User.find_for_twitter_oauth(request.env["omniauth.auth"],current_user)
     if @user.persisted?
+      @user.skip_confirmation!
       flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Twitter"
       sign_in_and_redirect @user, :event => :authentication
     else
-      session["devise.twitter_data"] = request.env["omniauth.auth"]
+      session["devise.twitter_uid"] = request.env["omniauth.auth"]
       redirect_to new_user_registration_url
     end
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -51,6 +51,28 @@ class User < ActiveRecord::Base
   #   end
   # end
 
+  def self.find_for_twitter_oauth(auth, signed_in_resource=nil)
+    user = User.where(:provider => auth.provider, :uid => auth.uid).first
+    if user
+      return user
+    else
+      registered_user = User.where(:email => auth.uid + "@twitter.com").first
+      if registered_user
+        return registered_user
+      else
+
+        user = User.create(
+                           provider:auth.provider,
+                           uid:auth.uid,
+                           email:auth.uid+"@twitter.com",
+                           password:Devise.friendly_token[0,20],
+        )
+        # user.skip_confirmation!
+      end
+
+    end
+  end
+
   def self.new_with_session(params, session)
     super.tap do |user|
       if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
